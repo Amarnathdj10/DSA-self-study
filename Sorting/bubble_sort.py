1044939cc53f5949f8234600a58d5a4a539fb8a7
Amarnathdj10 <amarnathdj56@gmail.com>
2026-05-29 22:15:15 +0530

    day 3

---
+++ Sorting/bubble_sort.py
@@ -0,0 +1,9 @@
+arr = [5,2,8,1,3]
+
+n = len(arr)
+
+for i in range(n):
+    for j in range(n-i-1):
+        if arr[j] > arr[j+1]:
+            arr[j],arr[j+1] = arr[j+1],arr[j]
+print(arr)
---
+++ Sorting/largest_element_after_sorting.py
@@ -0,0 +1,4 @@
+arr = [5,2,8,1,3]
+
+arr.sort(reverse=True)
+print(arr[0])
